54087dc4299b0d03b562a560ee3cb7943216514d
Alina Mikhailova <mihailovaalina777@yandex.ru>
2019-11-20 10:46:57 +0900

    Get fractions of nucleotides instead of rates

--- AllTermites/PCA.R
+++ AllTermites/PCA.R
@@ -1,9 +1,28 @@
 
-mut = read.table('results/4foldSubsRatesBrLen02.txt', header=TRUE, sep='\t')
+mut = read.table('results/4foldSubsRatesBrLen02Residuals.txt', header=TRUE, sep='\t')
 
-MATRIX = mut[, c(27:38)] # residuals
-MATRIX = mut[, c(2:13)] # subs number
-row.names(MATRIX)=mut$Species
+a = matrix(0, ncol=12, nrow=nrow(mut))
+
+mut$sumOfSubs = mut[, "C_T"] + mut[, "A_G"] + mut[, "T_C"] + mut[, "G_T"] + mut[, "A_C"] +
+  mut[, "G_A"] + mut[, "A_T"] + mut[, "G_C"] + mut[, "C_A"] + mut[, "C_G"] + mut[, "T_A"] +
+  mut[, "T_G"]
+
+for(i in 1:12){
+  a[, i] = mut[, i+1] / mut$sumOfSubs
+}
+
+fractions = as.data.frame(a)
+names(fractions) = sub(' ', '', paste(names(mut[, 2:13]), '_fr'))
+
+mutFr = cbind(mut, fractions)
+
+write.table(mutFr[, c(1:14, 39:51)], 'results/4foldSubsBrLen02Fractions.txt')
+
+# MATRIX = mut[, c(27:38)] # residuals
+# MATRIX = mut[, c(2:13)] # subs number
+MATRIX = mutFr[, c(40:51)]
+
+row.names(MATRIX)=mutFr$Species
 matrix = MATRIX
 
 PCA = prcomp(matrix, center = TRUE, scale = TRUE) #FALSE) # I don't scale because we analyze the same units (fraction from MutSpec) 
@@ -13,9 +32,6 @@ MATRIX$Pca1 = PCA$x[,1]
 MATRIX$Pca2 = PCA$x[,2]
 MATRIX$Pca3 = PCA$x[,3]
 MATRIX$Pca4 = PCA$x[,4]
-
-MATRIX = cbind(MATRIX, MUT[, 'Family'])
-names(MATRIX)[17] = 'Family'
 
 pdf('results/PCA.pdf')
 biplot(PCA, choices=c(1,2), col = c('white','black'), cex = 0.8) #  biplot(princomp(USArrests),choices=c(1,3))
--- AllTermites/getResidualsWithoutBrLen.R
+++ AllTermites/getResidualsWithoutBrLen.R
@@ -2,12 +2,13 @@
 
 mut = read.table('results/4foldSubsRatesBrLen02.txt', header=TRUE, sep='\t')
 
-plot(mut$C_T_rate, mut$BranchLength)
-
 lin_model = lm(C_T_rate ~ BranchLength, mut)
 summary(lin_model)
 
 lin_model$residuals
+
+plot(mut$C_T_rate, mut$BranchLength)
+abline(lm(mut$C_T_rate ~ mut$BranchLength))
 
 res = as.data.frame(matrix(nrow = nrow(mut)))
 for(i in 15:26){
@@ -21,11 +22,25 @@ names(res) = sub(' ', '', paste(names(mut[15:26]), '.res'))
 
 mut = cbind(mut, res)
 
+pdf('results/residuals.pdf')
+plot(mut$C_T_rate, mut$BranchLength)
+abline(lm(mut$C_T_rate ~ mut$BranchLength))
+
 plot(mut$C_T_rate.res, mut$BranchLength)
+abline(lm(mut$C_T_rate.res ~ mut$BranchLength))
+dev.off()
+
+# rate != 0
+notZero = mut[mut$C_T_rate != 0,]
+plot(notZero$C_T_rate, notZero$BranchLength)
+abline(lm(notZero$C_T_rate ~ notZero$BranchLength))
+lm(notZero$C_T_rate ~ notZero$BranchLength)
+
+# plot(mut$C_T_rate.res, mut$BranchLength)
 plot(mut$A_G_rate, mut$BranchLength)
 plot(mut$A_G_rate.res, mut$BranchLength)
 
 cor.test(mut$A_G_rate, mut$BranchLength)
 cor.test(mut$A_G_rate.res, mut$BranchLength)
 
-write.table(mut, 'results/4foldSubsRatesBrLen02Residuals.txt', row.names = FALSE, quote = FALSE)
+write.table(mut, 'results/4foldSubsRatesBrLen02Residuals.txt', row.names = FALSE, quote = FALSE, sep='\t')
