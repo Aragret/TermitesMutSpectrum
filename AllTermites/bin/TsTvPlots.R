537244ebec68cbdf911e890b0434de552794b161
Alina Mikhailova <mihailovaalina777@yandex.ru>
2019-12-24 11:42:24 +0900

    Ts/Tv for different groups

--- AllTermites/bin/TsTvPlots.R
+++ AllTermites/bin/TsTvPlots.R
@@ -48,26 +48,33 @@ mut$TsTv = (mut$C_T + mut$A_G + mut$T_C + mut$G_A) / (mut$G_T + mut$A_T + mut$G_
                                                         mut$A_C + mut$C_A + mut$C_G +
                                                         mut$T_A + mut$T_G)
 
+workers = mut[mut$Worker == 1,]
+workers = workers[!is.na(workers$Worker),]
+withoutWorkers = mut[(mut$Worker == 0) & !is.na(mut$Worker),]
+
 filter_workers = rbind(workers, withoutWorkers)
 
 summary(filter_workers$TsTv)
 
 pdf('../results/cockroaches11_19/TsTsWorkersSoldiers.pdf')
 
-ggplot(filter_workers, aes(TsTv)) +
-  geom_histogram(aes(fill = filter_workers$Worker), alpha = 0.3)
+ggplot(filter_workers, aes(TsTv, fill = filter_workers$Worker)) +
+  # geom_histogram(aes(fill = filter_workers$Worker), alpha = 0.4) +
+  geom_histogram( color="#e9ecef", alpha=0.4, position = 'dodge') +
+  scale_fill_manual(values=c("#404080", "#69b3a2"))
 
-ggplot(filter_workers, aes(TsTv)) +
-  geom_density(aes(fill = filter_workers$Worker), alpha = 0.3)
+
+# ggplot(filter_workers, aes(TsTv)) +
+#   geom_density(aes(fill = filter_workers$Worker), alpha = 0.3)
 
 # summary(mut$Soldier)
 filter_soldiers = mut[!is.na(mut$Soldier),]
 
-ggplot(filter_soldiers, aes(TsTv)) +
-  geom_histogram(aes(fill = filter_soldiers$Soldier), alpha = 0.3)
+ggplot(filter_soldiers, aes(TsTv, fill = filter_soldiers$Soldier)) +
+  # geom_histogram(aes(fill = filter_workers$Worker), alpha = 0.4) +
+  geom_histogram( color="#e9ecef", alpha=0.4, position = 'dodge') +
+  scale_fill_manual(values=c("#404080", "#69b3a2"))
 
-ggplot(filter_soldiers, aes(TsTv)) +
-  geom_density(aes(fill = filter_soldiers$Soldier), alpha = 0.3)
 
 dev.off()
 
@@ -92,15 +99,14 @@ summary(mut$Cockroaches)
 
 pdf('../results/cockroaches11_19/TsTvCockroachesVsTermites.pdf')
 
-ggplot(mut, aes(TsTv, fill = mut$Cockroaches, stat(count))) +
-  geom_density(alpha = 0.3)
-
 ggplot(mut, aes(TsTv, fill = mut$Cockroaches)) +
-  geom_density(alpha = 0.3)
-
-ggplot(mut, aes(TsTv, fill = mut$Cockroaches)) +
-  geom_histogram(alpha = 0.3)
+  # geom_histogram(aes(fill = filter_workers$Worker), alpha = 0.4) +
+  geom_histogram( color="#e9ecef", alpha=0.4, position = 'dodge') +
+  scale_fill_manual(values=c("#69b3a2", "#404080"))
 
 summary(mut$sumOfSubs)
+
+plot_hist(mut[mut$Cockroaches == 1, 2:13], 'cockroaches')
+plot_hist(mut[mut$Cockroaches == 0, 2:13], 'termites')
 
 dev.off()
