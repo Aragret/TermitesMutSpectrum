1803fe293bb790352e6bd6b178529a6745815287
Alina Mikhailova <mihailovaalina777@yandex.ru>
2020-01-08 16:03:09 +0900

    collapsed short branches

--- AllTermites/bin/SubsTables.Cockroaches.R
+++ AllTermites/bin/SubsTables.Cockroaches.R
@@ -3,20 +3,20 @@ rm(list=ls(all=TRUE))
 library(ggplot2)
 library(ggpubr)
 
-codonTable = read.table('../results/cockroaches11_19/PolarizeMutations.CodonsTable.Cockroaches.txt', sep='\t', header=TRUE)
+codonTable = read.table('../results/cockroaches11_19/mod_PolarizeMutations.CodonsTable.Cockroaches.txt', sep='\t', header=TRUE)
 
 withoutGapsCodonTable = codonTable[as.character(codonTable$DescendantCodon) != '---',]
 
-length(unique(codonTable$Species)) #677
-length(unique(withoutGapsCodonTable$Species)) #626
+length(unique(codonTable$Species)) # 567
+length(unique(withoutGapsCodonTable$Species)) # 567
 
-setdiff(codonTable$Species, withoutGapsCodonTable$Species)
+# setdiff(codonTable$Species, withoutGapsCodonTable$Species)
 
 FirstCodon = withoutGapsCodonTable$AncestorCodon
 SecondCodon = withoutGapsCodonTable$DescendantCodon
 
-data = data.frame(FirstCodon,SecondCodon)
-data$FirstSecond = paste(data$FirstCodon,data$SecondCodon,sep = '_')
+data = data.frame(FirstCodon, SecondCodon)
+data$FirstSecond = paste(data$FirstCodon, data$SecondCodon,sep = '_')
 
 COMPAR<-function(x)	{
   cod1 <- unlist(strsplit(x,'_'))[1]
@@ -45,22 +45,22 @@ COMPAR<-function(x)	{
   return(nucleotides);
 }
 
-data$Subst = apply(as.matrix(data$FirstSecond),1,COMPAR)  
+data$Subst = apply(as.matrix(data$FirstSecond), 1, COMPAR)  
 withoutGapsCodonTable$Subs = data$Subst
 
 tableSubs = withoutGapsCodonTable[withoutGapsCodonTable$Subs != 'MoreThanOne_SUBST',]
 
-length(unique(tableSubs$Species)) #624
+length(unique(tableSubs$Species)) #567
 
 #### all subs
 
 tableSubs$Subs = as.character(tableSubs$Subs)
 mut = tableSubs[which(!(tableSubs$Subs %in% c('A_N', 'A_R', 'C_N', 'G_N', 'T_K', 'T_N', 'T_Y',
-                                              '-_A', '-_C', 'C_Y', '-_G', 'G_R', '-_T'))),]
+                                              '-_A', '-_C', 'C_Y', '-_G', 'G_R', '-_T', 'A_Y'))),]
 
-length(unique(mut$Species)) #623
+length(unique(mut$Species)) # 567
 
-pdf('../results/cockroaches11_19/HistOfSubs.pdf')
+pdf('../results/cockroaches11_19/mod_HistOfSubs.pdf')
 
 ggplot(mut, aes(x = Subs)) +
   geom_histogram(stat = 'count')
@@ -105,9 +105,9 @@ VecOfSynFourFoldDegenerateSites <- c('CTT', 'CTC', 'CTA', 'CTG',
 length(unique(VecOfSynFourFoldDegenerateSites)) # 36
 
 mut4f = mut[mut$AncestorCodon %in% VecOfSynFourFoldDegenerateSites & mut$DescendantCodon %in% VecOfSynFourFoldDegenerateSites,]; nrow(mut4f) # 209120
-# ~100k subs
+# ~300k subs
 
-length(unique(mut4f$Species)) #593
+length(unique(mut4f$Species)) #567
 
 Temp = mut4f
 Temp$number = 1
@@ -117,7 +117,7 @@ Agg$Number = Agg$Number/sum(Agg$Number)
 ggbarplot(Agg, 'Subs', 'Number', xlab="Substitution types", title = '4-fold degenerate sites', 
           fill = 'Subs', color = 'Subs', palette = c("#bdbdbd", "#7fcdbb", "#bdbdbd", "#bdbdbd", "#bdbdbd", "#feb24c", "#f03b20", "#bdbdbd", "#bdbdbd", "#bdbdbd", "#2c7fb8", "#bdbdbd"), combine = TRUE)
 
-write.table(mut4f, '../results/cockroaches11_19/4foldSubs.Cockroaches.txt', sep='\t', row.names = FALSE, quote = FALSE)
+write.table(mut4f, '../results/cockroaches11_19/mod_4foldSubs.Cockroaches.txt', sep='\t', row.names = FALSE, quote = FALSE)
 
 table(mut4f$Species)
 
