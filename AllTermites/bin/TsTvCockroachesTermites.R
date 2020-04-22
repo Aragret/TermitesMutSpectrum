410a59b427b08da351347b1648762a9a7bf5aab3
Alina Mikhailova <mihailovaalina777@yandex.ru>
2020-04-22 17:36:26 +0300

    Delete some unnecessary stuff

--- AllTermites/bin/TsTvCockroachesTermites.R
+++ AllTermites/bin/TsTvCockroachesTermites.R
@@ -104,27 +104,5 @@ summary(Result)
 #   Cockroaches1    -9.446e-02  2.154e-02  -4.386 1.16e-05 ***
 #   Tv:Cockroaches1 -2.095e-03  1.165e-04 -17.975  < 2e-16 ***
 
-Result <- glm(Ts ~ Tv + Cockroaches + BranchLength, data = mut, family = poisson)
-summary(Result)
-
-# Coefficients:
-#   Estimate Std. Error z value Pr(>|z|)    
-# (Intercept)   4.592e+00  6.969e-03 659.013   <2e-16 ***
-#   Tv            2.364e-03  5.921e-05  39.924   <2e-16 ***
-#   Cockroaches1 -4.285e-01  1.225e-02 -34.980   <2e-16 ***
-#   BranchLength -5.509e-02  5.822e-02  -0.946    0.344    
-
-Result <- glm(Ts ~ (Tv + Cockroaches) * BranchLength, data = mut, family = poisson)
-summary(Result)
-
-# Coefficients:
-#   Estimate Std. Error z value Pr(>|z|)    
-# (Intercept)                4.455e+00  1.199e-02 371.509   <2e-16 ***
-#   Tv                         2.376e-03  7.844e-05  30.294   <2e-16 ***
-#   Cockroaches1              -3.919e-01  2.205e-02 -17.775   <2e-16 ***
-#   BranchLength               3.101e+00  2.251e-01  13.778   <2e-16 ***
-#   Tv:BranchLength           -8.118e-03  8.570e-04  -9.473   <2e-16 ***
-#   Cockroaches1:BranchLength -4.691e-01  1.874e-01  -2.503   0.0123 *  
-
 
 dev.off()
