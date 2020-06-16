d018487f4996411c14ca5f02a5a7da5c3f8a06e5
Alina Mikhailova <mihailovaalina777@yandex.ru>
2020-06-16 20:13:57 +0300

    Extracted ATGC content in 4-fold deg sites for all genes (major and minor)

---
+++ AllTermites/bin/getNuclContentEachGene4fold.nucleotide_content.R
@@ -0,0 +1,65 @@
+rm(list=ls(all=TRUE))  # remove everything from R memory (old variables, datasets...) 
+
+library("Biostrings")
+library("seqinr")
+library(dplyr)
+
+current_dir = getwd()
+
+align_dir = '../../AllTermites/data/forAlign/'
+
+setwd(align_dir)
+
+filelist <- list.files()
+
+VecOfSynFourFoldDegenerateSites <- c('CTT', 'CTC', 'CTA', 'CTG', 
+                                     'GTT', 'GTC', 'GTA', 'GTG', 
+                                     'TCT', 'TCC', 'TCA', 'TCG', 
+                                     'CCT', 'CCC', 'CCA', 'CCG', 
+                                     'ACT', 'ACC', 'ACA', 'ACG', 
+                                     'GCT', 'GCC', 'GCA', 'GCG', 
+                                     'CGT', 'CGC', 'CGA', 'CGG', 
+                                     'GGT', 'GGC', 'GGA', 'GGG',
+                                     'AGT', 'AGC', 'AGA', 'AGG')
+
+majorGenes = c('ATP6', 'ATP8', 'COI', 'COII', 'COIII', 'CyB', 'ND3', 'ND6', 'ND2')
+minorGenes = c('ND1', 'ND4', 'ND4L', 'ND5')
+
+final = c()
+
+for(f in filelist){
+  one_line = c()
+  # f = "ATP6.fasta"
+  geneName = sub('.fasta', '', f)
+  genes = read.fasta(f, as.string = TRUE)
+  genes_df = data.frame(Species=names(genes), Seqs=unlist(getSequence(genes, as.string=T)))
+  genes_df$Codons = lapply(as.character(genes_df$Seqs), s2c)
+  genes_df$Codons = lapply(genes_df$Codons, splitseq)
+  for(i in 1:nrow(genes_df)){
+    # i = 1
+    speciesName = as.character(genes_df$Species[i])
+    codons = unlist(genes_df$Codons[i])
+    numbers4f = which(toupper(codons) %in% VecOfSynFourFoldDegenerateSites)
+    codons4f = toupper(codons[numbers4f])
+    lastLetter = substring(codons4f, first = 3, last = 3)
+    a = sum(nchar(gsub("[^A]", "", lastLetter)))
+    t = sum(nchar(gsub("[^T]", "", lastLetter)))
+    g = sum(nchar(gsub("[^G]", "", lastLetter)))
+    c = sum(nchar(gsub("[^C]", "", lastLetter)))
+    one_line = rbind(one_line, c(speciesName, geneName, a, t, g, c))
+  }
+  one_line = as.data.frame(one_line)
+  names(one_line) = c('Species', 'Gene', 'A', 'T', 'G', 'C')
+  if(f == "ATP6.fasta"){
+    final = one_line
+  }
+  else{
+    final = full_join(final, one_line)
+  }
+}
+
+data = final %>% mutate(MajorStrand = case_when(.$Gene %in% majorGenes ~ 1, 
+                                         .$Gene %in% minorGenes ~ 0))
+
+write.table(data, '../../results/nucleotide_content06_20/ATGCforEachGene4fold.txt',
+            sep = '\t', row.names = FALSE, quote = FALSE)
