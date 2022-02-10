b15b9105228b55a19e06d330b56d824c398cdfca
Gleb <glebosadchiy71@gmail.com>
2022-02-10 23:49:57 +0200

    Tweak scripts

--- Head/2Scripts/Polymorphisms.Normalization.R
+++ Head/2Scripts/Polymorphisms.Normalization.R
@@ -181,10 +181,13 @@ dev.off()
 library("ggpubr")
 library(cowplot)
 
-NormFrac = read.table("../../Body/3Results/Cockroaches.Polymorphisms.Normalization.Normalized12Fractions.txt", header = TRUE)
+NormFrac = read.table("/home/glebo/Documents/lab/TermitesMutSpectrum/Body/3Results/Cockroaches.Polymorphisms.Normalization.Normalized12Fractions.txt", header = TRUE)
 
 
-pdf("../../Body/4Figures/Cockroaches.Polymorphisms.Normalization.R.03.Bars.pdf", width = 15, height = 10)
+pdf("/home/glebo/Documents/lab/TermitesMutSpectrum/Body/4Figures/Cockroaches.Polymorphisms.Normalization.R.03.Bars.pdf", width = 15, height = 10)
+
+b<-ggplot(NormFrac, aes(x="Subs", y="Normalised2Number"), xlab="Substitution types", ylab="Normalised frequencies")
+b
 
 NormFrac$Subs = as.factor(NormFrac$Subs) 
 
@@ -192,6 +195,7 @@ a = ggbarplot(NormFrac, 'Subs', 'Normalised2Number', xlab="Substitution types",
               ylab = 'Normalized frequencies', title = 'all',
               fill = 'Subs', color = 'Subs', palette = c("#bdbdbd", "#7fcdbb", "#bdbdbd", "#bdbdbd", "#bdbdbd", "#feb24c", "#f03b20", "#bdbdbd", "#bdbdbd", "#bdbdbd", "#2c7fb8", "#bdbdbd"), combine = TRUE) +
   scale_x_discrete(labels = sort(unique(NormFrac$Subs), decreasing = TRUE))
+a + theme(legend.position = "none")
 
 b = ggbarplot(NormFrac[NormFrac$Family == 'Cryptocercidae',], 'Subs', 'Normalised2Number', 
               xlab="Substitution types", ylab = 'Normalized frequencies', title = 'Cryptocercidae',
@@ -261,51 +265,110 @@ save_plot('~/Alina/other_projects/TermitesMutSpectrum/Body/4Figures/Polymorphism
 
 library(dplyr)
 
-mut = read.table('../../Body/3Results/Polymorphisms.MutSpecData.txt', header = TRUE)
-
+mutCock = read.table('~/Documents/lab/TermitesMutSpectrum/Body/3Results/Cockroaches.MutSpecData.txt', header = TRUE)
+mutTer = read.table('~/Documents/lab/TermitesMutSpectrum/Body/3Results/Termites.MutSpecData.txt', header = TRUE)
 # External Internal 
 # 662     1013
 
 # mann-whitney test for normilized mutspec
-
-normMut = mut %>%
+test_prep <- function(mut) {
+  normMut = mut %>%
   group_by(Species) %>% 
   mutate(
     AllSubs = length(Subs)
-  ) %>%
-  filter(Subs == 'T_C') %>% 
+  ) %>% 
   mutate(
-    T_C = length(Subs) / AllSubs
+    A_T = sum(Subs == "A_T") / AllSubs,
+    A_G = sum(Subs == "A_G") / AllSubs,
+    A_C = sum(Subs == "A_C") / AllSubs,
+    T_A = sum(Subs == "T_A") / AllSubs,
+    T_G = sum(Subs == "T_G") / AllSubs,
+    T_C = sum(Subs == "T_C") / AllSubs,
+    G_T = sum(Subs == "G_T") / AllSubs,
+    G_A = sum(Subs == "G_A") / AllSubs,
+    G_C = sum(Subs == "G_C") / AllSubs,
+    C_T = sum(Subs == "C_T") / AllSubs,
+    C_G = sum(Subs == "C_G") / AllSubs,
+    C_A = sum(Subs == "C_A") / AllSubs,
   ) %>% 
   distinct(Species, .keep_all = TRUE) %>%
   mutate(
+    A_fr = A / (A + T + G + C),
     T_fr = T / (A + T + G + C),
-    T_C_norm = T_C / T_fr
+    G_fr = G / (A + T + G + C),
+    C_fr = C / (A + T + G + C),
+    T_A_norm = T_A / T_fr,
+    T_C_norm = T_C / T_fr,
+    T_G_norm = T_G / T_fr,
+    A_T_norm = A_T / A_fr,
+    A_C_norm = A_C / A_fr,
+    A_G_norm = A_G / A_fr,
+    G_A_norm = G_A / G_fr,
+    G_C_norm = G_C / G_fr,
+    G_T_norm = G_T / G_fr,
+    C_A_norm = C_A / C_fr,
+    C_T_norm = C_T / C_fr,
+    C_G_norm = C_G / C_fr,
+    # Geoscapheus_robustus has 0 G_fr
+    T_C_norm_fr = T_C_norm / (T_A_norm + T_C_norm + T_G_norm + A_T_norm + A_C_norm + A_G_norm + G_A_norm + G_C_norm + G_T_norm + C_A_norm + C_T_norm + C_G_norm)
   )
+}
 
-t.test(normMut[normMut$Family == 'Termitidae',]$T_C_norm, normMut[normMut$Family != 'Termitidae',]$T_C_norm)
+normMutCock <- test_prep(mutCock)
+normMutTer <- test_prep(mutTer)
+normMutCock[6, "T_C_norm_fr"] = 0 #wtf do I do with that?
+t.test(normMutTer$T_C_norm_fr, normMutCock$T_C_norm_fr)
+
+
+####################
+par(las=2) # make label text perpendicular to axis
+par(mar = c(10,4,4,2) + 2) # increase y-axis margin.
+barplot(normMutCock$G, names.arg = normMutCock$Species, )
+
+dev.off()
+#####################
 
 # 20 vs 
 
-# t = 3.664, df = 30.091, p-value = 0.0009496
+# t = 8.3365, df = 53.696, p-value = 2.957e-11
 # alternative hypothesis: true difference in means is not equal to 0
 # 95 percent confidence interval:
-#   0.9909639 3.4861512
+# 0.1800588 0.2941073
 # sample estimates:
 #   mean of x mean of y 
-# 4.973166  2.734609
+# 0.32780823 0.09072514 
 
 
-wilcox.test(normMut[normMut$Family == 'Termitidae',]$T_C_norm, normMut[normMut$Family != 'Termitidae',]$T_C_norm,
+wilcox.test(normMutTer$T_C_norm, normMutCock$T_C_norm,
             conf.int = TRUE)
 
-# W = 235, p-value = 0.00387
+# W = 642, p-value = 6.261e-11
 # alternative hypothesis: true location shift is not equal to 0
 # 95 percent confidence interval:
-#   0.7241917 3.4648685
+#   1.811509 3.183974
 # sample estimates:
 #   difference in location 
-# 1.855148 
+# 2.490454  
+
+# aov
+
+aov_prep <- data.frame(T_C_norm = normMutTer[normMutTer$Family == 'Termitidae',]$T_C_norm_fr, Group = "Higher Ter")
+aov_prep <- rbind(aov_prep, data.frame(T_C_norm = normMutTer[normMutTer$Family != 'Termitidae',]$T_C_norm_fr, Group = "Lower Ter"))
+aov_prep <- rbind(aov_prep, data.frame(T_C_norm = normMutCock$T_C_norm_fr, Group = "Cockroach"))
+
+aov.res <- aov(T_C_norm ~ Group, data = aov_prep) ## Google about it
+
+summary(aov.res)
+#             Df Sum Sq Mean Sq F value   Pr(>F)    
+# Group        2  153.3   76.67   30.25 2.18e-09 ***
+# Residuals   51  129.3    2.53                     
+# ---
+#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+TukeyHSD(aov.res)
+#                        diff        lwr        upr     p adj
+# Higher Ter-Cockroach  3.953346  2.7220573  5.1846347 0.0000000
+# Lower Ter-Cockroach   1.714788  0.3872827  3.0422942 0.0082643
+# Lower Ter-Higher Ter -2.238558 -3.5513401 -0.9257751 0.0004082
 
 # fisher test
 
